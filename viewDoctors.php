f9174c40361625ca0fffeb67c7b4153d01ccb691
lauren <laurenphan13@gmail.com>
2019-11-15 18:32:20 -0500

    styling

--- viewDoctors.php
+++ viewDoctors.php
@@ -7,7 +7,7 @@
           $stmt->bind_param(s, $searchString);
           $stmt->execute();
           $stmt->bind_result($staff_id, $name, $age);
-          echo "<table border=1><th>Staff ID</th><th>Name</th><th>Age</th>\n";
+          echo "<br><table border=1><th>Staff ID</th><th>Name</th><th>Age</th>\n";
           while($stmt->fetch()) {
                   echo "<tr><td>$staff_id</td><td>$name</td><td>$age</td></tr>";
           }
